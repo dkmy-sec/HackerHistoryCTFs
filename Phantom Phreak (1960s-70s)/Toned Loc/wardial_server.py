581d28050ebd98b2e78ec9cfaf144125b58e6377
KLewis <klewdesign@gmail.com>
2025-09-04 11:52:04 -0400

    Just finished up SQL Slam (2000s) challenge.  Uploading slammer.py and users.db

---
+++ Phantom Phreak (1960s-70s)/Toned Loc/wardial_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Pure TCP "line: simulator for ToneLoc-in-DOSBox-X
+#
+
+
+MAPPINGS = {
+
+}
+
+
+
+BANNER = {
+
+}
+
+
+def handle(conn, addr, role):
+    if role == "BUSY"
